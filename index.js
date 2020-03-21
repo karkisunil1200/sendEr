c75819b7cb22c55efb612d5b155a13e5be4e3613
Sunil Karki <karkisunil1200@gmail.com>
2020-03-21 18:26:55 -0500

    Changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'There'});
+  res.send(`<h2>Welcome to the homepage</h2>`);
 });
 
 const PORT = process.env.PORT || 5000;
